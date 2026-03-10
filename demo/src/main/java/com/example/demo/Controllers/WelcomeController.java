e286ec12e79b6740a06f143c3ff6f6a6357bbf69
Angel Johnsen <daffyangel1210@gmail.com>
2026-03-10 14:15:02 +0100

    Added controller

--- demo/src/main/java/com/example/demo/Controllers/WelcomeController.java
+++ demo/src/main/java/com/example/demo/Controllers/WelcomeController.java
@@ -1,4 +1,4 @@
-package Controllers;
+package com.example.demo.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
